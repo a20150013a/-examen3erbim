a30d1f96c6a653dd5a94ec4f9df1755702707d61
a20150013a <131173051+a20150013a@users.noreply.github.com>
2023-09-26 08:44:38 -0500

    Add files via upload

---
+++ examen.js
@@ -0,0 +1,19 @@
+var nombre = prompt("Escribe tu nombre") 
+
+
+
+
+alert("Bienvenido a al cuestionario sobre los mares")
+
+var p1 = prompt("¿Cuánta agua hay en la Tierra?\na:Alrededor de un 41% de la superficie terrestre es agua\nb:Alrededor de un 51% de la superficie terrestre es agua\nc: Alrededor de un 71% de la superficie terrestre es agua") 
+if 
+
+
+
+var p2 = prompt("¿Qué océano es el más grande?\na:El Pacífico\nb:el atlantico\nc:El Indico")
+
+
+
+
+var p3 = prompt
+
